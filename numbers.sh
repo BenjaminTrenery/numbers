1fe1a1f4c4a07ad0705fe33cd08cc3084728eab9
BenjaminTrenery <trenery@chapman.edu>
2019-11-17 20:33:33 -0800

    Adds numbers.sh

---
+++ numbers.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+# numbers.sh
+# Benjamin Trenery
+echo "Enter a positive integer"
+read NUM
+N=1
+while [ $NUM -ge $N ]
+do
+	if (( $N % 2 == 0 ))
+	then
+		echo "$N even"
+	else
+		echo "$N odd"
+	fi
+
+        N=$[$N+1]
+done
+
